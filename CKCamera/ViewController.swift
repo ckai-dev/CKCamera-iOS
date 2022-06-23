40087944fa3db1fcb0b8c14bd0a52547b97ed086
Kai Chen <xmkevinchen@users.noreply.github.com>
2022-06-23 14:22:00 -0500

    Initial Commit

---
+++ CKCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CKCamera
+//
+//  Created by Kai Chen on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
